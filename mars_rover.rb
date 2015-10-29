0009933fd16903465c595a27289d648ff56921c3
Matt <matt.cavdar@gmail.com>
2015-10-29 16:08:42 -0400

    Add move method for all directions and positions

--- mars_rover.rb
+++ mars_rover.rb
@@ -9,50 +9,72 @@ class Rover
 
 	def read_instruction(input)
 		if input == "L" || input == "R"
-
+			turn(input)
+		elsif input == "M"
+			move
+		else
+			"DOES NOT COMPUTE"
+		end
 	end
 
 	def turn(direction)
-			if @dir == "N"
-				if direction == "L"
-					@dir = "W"
-				elsif direction == "R"
-					@dir = "E"
-				else
-					puts "DOES NOT COMPUTE"
-				end
-
-			if @dir == "W"
-				if direction == "L"
-					@dir = "S"
-				elsif direction == "R"
-					@dir = "N"
-				else
-					puts "DOES NOT COMPUTE"
-				end
-
-			if @dir == "S"
-				if direction == "L"
-					@dir = "E"
-				elsif direction == "R"
-					@dir = "W"
-				else
-					puts "DOES NOT COMPUTE"
-				end
-
-			if @dir == "E"
-				if direction == "L"
-					@dir = "N"
-				elsif direction == "R"
-					@dir = "S"
-				else
-					puts "DOES NOT COMPUTE"
-				end
+		if @dir == "N"
+			if direction == "L"
+				@dir = "W"
+			elsif direction == "R"
+				@dir = "E"
+			else
+				puts "DOES NOT COMPUTE"
 			end
 
+		elsif @dir == "W"
+			if direction == "L"
+				@dir = "S"
+			elsif direction == "R"
+				@dir = "N"
+			else
+				puts "DOES NOT COMPUTE"
+			end
 
+		elsif @dir == "S" #Needed to use elsif because it is an if within an if
+			if direction == "L"
+				@dir = "E"
+			elsif direction == "R"
+				@dir = "W"
+			else
+				puts "DOES NOT COMPUTE"
+			end
+
+		elsif @dir == "E"
+			if direction == "L"
+				@dir = "N"
+			elsif direction == "R"
+				@dir = "S"
+			else
+				puts "DOES NOT COMPUTE"
+			end
+		else
+			puts "DOES NOT COMPUTE"
+		end
+	end
+
+	def move
+		if @dir == "N"
+			@pos_y += 1
+		elsif @dir == "S"
+			@pos_y -= 1
+		elsif @dir == "W"
+			@pos_x -= 1
+		elsif @dir == "E"
+			@pos_x += 1
+		else
+			puts "DOES NOT COMPUTE"
+		end
 
 	end
+
 end
+
+
 rover1 = Rover.new(1,2,"N")
 
